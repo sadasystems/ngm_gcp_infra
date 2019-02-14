2944853afb6aea1bea9099c66c4ecbff3d24263a
George Lerma <george.lerma@sadasystems.com>
2019-02-13 20:08:14 -0600

    initial checkin and organization of dirs and files

---
+++ Terraform/Tier3/GCSBucket/README.md
@@ -0,0 +1,13 @@
+# Terraform Component
+
+Bucket
+
+## Description
+
+TODO
+
+
+---
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ Terraform/Tier3/GCSBucket/main.tf
@@ -0,0 +1,9 @@
+module "bucket" {
+  source           = "git@github.com:sadasystems/gcp_tf_modules.git//gcp/gcs_bucket/v1?ref=feature/NG-50-gcs-bucket"
+  project_id       = "${var.project_id}"
+  name             = "${var.name}"
+  region           = "${var.region}"
+  storage_class = "${var.storage_class}"
+  lifecycle_rules = "${var.lifecycle_rules}"
+  role_entities = "${var.role_entities}"
+}
